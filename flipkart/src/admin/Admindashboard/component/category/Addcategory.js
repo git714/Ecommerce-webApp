fdd551fc7a4907fdcec0f444f55c61de0b3b0f7e
pallove kaushik <84062202+git714@users.noreply.github.com>
2022-04-13 14:00:39 +0530

    Update Addcategory.js

--- flipkart/src/admin/Admindashboard/component/category/Addcategory.js
+++ flipkart/src/admin/Admindashboard/component/category/Addcategory.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom';
 
 import { useForm } from "react-hook-form";
 import axios from 'axios';
 
+
 export const Addcategory = () => {
-  // const Navigate = useNavigate();
+   const navigate = useNavigate();
   
     const {register,handleSubmit}=useForm();
 const onSubmit=(data)=>{
@@ -13,7 +14,7 @@ const onSubmit=(data)=>{
     axios.post('http://localhost:5000/users/category/create', data)
     .then((res) => {
 
-      // history.push("/home");
+      navigate("/admindashboard")
 
     }).catch((error) => {
         console.log(error)
@@ -24,14 +25,15 @@ const onSubmit=(data)=>{
 
   return (
     <><div className="container">
-    <h2>Horizontal form</h2>
+    <h2>Add new Category</h2>
     <form className="form-horizontal" onSubmit={handleSubmit(onSubmit)} >
       <div className="form-group">
-        <label className="control-label col-sm-2" htmlFor="category">Category Name:</label>
+        <label className="control-label col-sm-2" htmlFor="category">Name:</label>
         <div className="col-sm-10">
           <input {...register("name",{required:true})} type="text" className="form-control" id="name" placeholder="Enter the category name" name="name"/>
         </div>
       </div>
+     
      
       <div className="form-group">        
         <div className="col-sm-offset-2 col-sm-10">
@@ -45,4 +47,5 @@ const onSubmit=(data)=>{
   </>
   )
 }
+
 
